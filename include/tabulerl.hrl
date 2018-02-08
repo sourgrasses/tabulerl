227d2917d9d0087133882e42a25fe4d63f7d1b97
Jennifer Wheeler <jwheeler@antiochcollege.edu>
2018-02-07 21:28:17 -0500

    starting to implement packet encoding stuff

---
+++ include/tabulerl.hrl
@@ -0,0 +1,38 @@
+-record(packet, {
+          sock = null,
+          usock = null,
+          itcp = null,
+          opts = null,
+          state = ready,
+          tail,
+          pack_header,
+          pack_data,
+          result = null,
+          query = null,
+          transaction = null,
+          env
+         }).
+
+%% types and statuses for packet headers
+-define(pack_sqlbatch, 16#01).
+-define(pack_oldlogin, 16#02).
+-define(pack_rpc, 16#03).
+-define(pack_result, 16#04).
+-define(pack_attention, 16#06).
+-define(pack_bulk_load, 16#07).
+-define(pack_federated_token, 16#08).
+-define(pack_manager_request, 16#0E).
+-define(pack_login, 16#10).
+-define(pack_sspi, 16#11).
+-define(pack_prelogin, 16#12).
+
+-define(status_normal, 16#00).
+-define(status_eom, 16#01).
+-define(status_reset, 16#08).
+-define(status_reset_skip, 16#10).
+
+-define(spid, 16#00:16).
+
+-define(window, 16#00).
+
+%% data types
--- src/tabulerl_encode.erl
+++ src/tabulerl_encode.erl
@@ -1,28 +1,24 @@
 -module(tabulerl_encode).
 
+-include("tabulerl.hrl").
+
 -export([]).
 
--record(packet, {
-          sock = null,
-          usock = null,
-          itcp = null,
-          opts = null,
-          state = ready,
-          tail,
-          pack_header,
-          pack_data,
-          result = null,
-          query = null,
-          transaction = null,
-          env
-         }).
+encode(prelogin, Opts) ->
+    Version = << 16#0B, 16#00, 16#0C, 16#38, 16#00, 16#00 >>,
+    V_size = bytesize(Version),
+    Offset = 16#06,
+    V_meta = << 16#00, Offset:16, V_size:16 >>,
+    Encryption = 16#00,
+    Terminator = 16#FF,
+    Data = << V_meta/binary, Terminator, Version/binary >>
+    Size = byte_size(Data),
+    Header = encode_header(?pack_prelogin, Size, Opts).
 
-%% @doc Encodes login information.
-login() ->
-    ok.
+encode_header(Type, Size, Opts) ->
+    Status = 1,
+    Id = 1,
+    << Type, Status, Size:16, ?spid, id, ?window >>.
 
-query() ->
-    ok.
-
-pack(Message) ->
+encode_packs(Type, Packs) ->
     ok.
